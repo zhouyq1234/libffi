57b3ccec397166b3039ee9d6b5e779e04168e883
Anthony Green <green@moxielogic.com>
2016-09-04 22:49:32 -0400

    xvfb already installed on travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,6 +12,6 @@ else
       sudo apt-get install gcc-multilib g++-multilib;
   fi
   if [ "$HOST" = i686-w64-mingw32 ] ; then
-      sudo apt-get install gcc-mingw-w64-i686 binutils-mingw-w64-i686 wine Xvfb;
+      sudo apt-get install gcc-mingw-w64-i686 binutils-mingw-w64-i686 wine;
   fi
 fi
