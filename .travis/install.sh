0b4e329255641b0000ab58a6a98f10917c85d51b
Anthony Green <green@moxielogic.com>
2016-09-04 22:40:11 -0400

    wine needs a virtual framebuffer

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,6 +12,8 @@ else
       sudo apt-get install gcc-multilib g++-multilib;
   fi
   if [ "$HOST" = i686-w64-mingw32 ] ; then
-      sudo apt-get install gcc-mingw-w64-i686 binutils-mingw-w64-i686 wine;
+      sudo apt-get install gcc-mingw-w64-i686 binutils-mingw-w64-i686 wine Xvfb;
+      # to run wine headlessly, we need a virtual frame buffer
+      Xvfb :0 -screen 0 1024x768x16 &;
   fi
 fi
